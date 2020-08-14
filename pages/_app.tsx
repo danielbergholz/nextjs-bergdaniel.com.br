83797767ab5724d6ca6169e128b54430f3061a91
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-08-14 19:57:03 -0300

    _document now on Typescript, and general meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 
 import { ThemeProvider } from '../context/theme';
 import NavBar from '../components/NavBar';
@@ -9,19 +8,9 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
-      <Head>
-        <title>Daniel Berg</title>
-        <link rel="shortcut icon" href="/logo.ico" type="image/x-icon" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <ThemeProvider>
-        <NavBar />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <NavBar />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,63 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+interface ReturnType {
+  styles: JSX.Element;
+  html: string;
+  head?: JSX.Element[];
+}
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<ReturnType> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="shortcut icon" href="/logo.ico" type="image/x-icon" />
+          <link rel="canonical" href="https://bergdaniel.com.br/" />
+          <meta property="og:image" content="/logo.ico" />
+          <meta property="og:type" content="website" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
