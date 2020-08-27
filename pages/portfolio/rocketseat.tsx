d40590c4a1200fc3631a6b406539d82313430943
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-08-27 20:25:51 -0300

    Add meta tags to rocketseat page

--- pages/portfolio/rocketseat.tsx
+++ pages/portfolio/rocketseat.tsx
@@ -1,23 +1,38 @@
 import Link from 'next/link';
+import Head from 'next/head';
 import { MdArrowBack } from 'react-icons/md';
 import Button from '../../components/Button';
 import { Container } from '../../styles/portfolio/rocketseat/styles';
 
 export default function Rocketseat(): JSX.Element {
   return (
-    <Container>
-      <img
-        src="/assets/certificado_rocketseat.jpg"
-        alt="Certificado rocketseat Daniel Bergholz"
-      />
-      <Link href="/portfolio">
-        <a>
-          <Button>
-            <MdArrowBack size={25} color="#fff" />
-            Voltar
-          </Button>
-        </a>
-      </Link>
-    </Container>
+    <>
+      <Head>
+        <title>Rocketseat | Portfolio | Daniel Berg</title>
+        <meta
+          name="og:title"
+          property="og:title"
+          content="Certificado Rocketseat"
+        />
+        <meta
+          name="description"
+          content="Certificado de conclusão do Bootcamp GoStack da Rocketseat (turma 11), incluindo os assuntos: Typescript, React, React Native e Node"
+        />
+      </Head>
+      <Container>
+        <img
+          src="/assets/certificado_rocketseat.jpg"
+          alt="Certificado rocketseat Daniel Bergholz"
+        />
+        <Link href="/portfolio">
+          <a>
+            <Button>
+              <MdArrowBack size={25} color="#fff" />
+              Voltar
+            </Button>
+          </a>
+        </Link>
+      </Container>
+    </>
   );
 }
